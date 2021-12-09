69f5460d02e475111ff2910d7567f4b691b5fe9e
xuyazhong <xuyazhong@foxmail.com>
2021-12-09 16:47:22 +0800

    ok

--- column.js
+++ column.js
@@ -3,18 +3,10 @@ import { StyleSheet, View, TouchableOpacity } from 'react-native';
 
 export default Column = (props) => {
 
-  function columnComponent() {
-    return (
-      <View style={ [props.style, { flexDirection: 'column' }]}>
-        {props.children}
-      </View>
-    )
-  }
-
-  return props.onPress ? (
-    <TouchableOpacity onPress={props.onPress}>
-      {columnComponent()}
-    </TouchableOpacity>
-  ) : columnComponent()
+  return (
+    <View style={[props.style, { flexDirection: 'column' }]}>
+      {props.children}
+    </View>
+  );
 
 }
--- row.js
+++ row.js
@@ -3,18 +3,10 @@ import { StyleSheet, View, TouchableOpacity } from 'react-native';
 
 export default Row = (props) => {
 
-  function rowComponent() {
-    return (
-      <View style={ [props.style, { flexDirection: 'row' }]}>
-        {props.children}
-      </View>
-    )
-  }
-
-  return props.onPress ? (
-    <TouchableOpacity onPress={props.onPress}>
-      {rowComponent()}
-    </TouchableOpacity>
-  ) : rowComponent()
-
+  return (
+    <View style={[props.style, { flexDirection: 'row' }]}>
+      {props.children}
+    </View>
+  );
+  
 }
